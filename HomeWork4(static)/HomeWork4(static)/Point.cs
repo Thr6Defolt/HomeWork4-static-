cac23d7a23c3f19daf2cbd9a56cd898217afa73e
Thr6Defolt <43175754+Thr6Defolt@users.noreply.github.com>
2023-10-16 11:24:39 +0300

    Add files via upload

---
+++ HomeWork4(static)/HomeWork4(static)/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_static_
+{
+    public class Point
+    {
+        private static int pointCount = 0;
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            pointCount++;
+        }
+
+        public static int GetPointCount()
+        {
+            return pointCount;
+        }
+
+        public double GetDistanceToPoint(Point otherPoint)
+        {
+            int dx = x - otherPoint.x;
+            int dy = y - otherPoint.y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public static double GetDistanceBetweenPoints(Point point1, Point point2)
+        {
+            int dx = point1.x - point2.x;
+            int dy = point1.y - point2.y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
---
+++ HomeWork4(static)/HomeWork4(static)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace HomeWork4_static_
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Point point1 = new Point(1, 2);
+            Point point2 = new Point(4, 6);
+            Point point3 = new Point(3, 1);
+
+            Console.WriteLine($"Number of points: {Point.GetPointCount()}");
+
+            double distance1 = point1.GetDistanceToPoint(point2);
+            double distance2 = point2.GetDistanceToPoint(point3);
+            double distance3 = Point.GetDistanceBetweenPoints(point1, point3);
+
+            Console.WriteLine($"Distance from point1 to point2: {distance1}");
+            Console.WriteLine($"Distance from point2 to point3: {distance2}");
+            Console.WriteLine($"Distance between point1 and point3: {distance3}");
+        }
+    }
+}
